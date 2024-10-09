9e3631ca75558494cb901f8b464d62be1f3f997b
biohzrdmx <hi@biohzrdmx.me>
2024-10-08 20:31:09 -0600

    Added rainbow mode as default, misc. fixes and improvements

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,9 @@
 
 #include <YetAnotherPcInt.h>
 #include <EasyButton.h>
-#include <RotaryEncoder.h>
 #include <TM1651.h>
 #include <FastLED.h>
+#include <Timer.h>
 
 #define BTN_DEBOUNCE    40
 
@@ -17,9 +17,10 @@
 
 #define LED_TYPE        WS2811
 #define COLOR_ORDER     GRB
-#define NUM_LEDS        17
+#define NUM_LEDS        8
 
 #define MODE_SOLID      0
+#define MODE_RAINBOW    1
 
 #define COLOR_RED       0
 #define COLOR_GREEN     1
@@ -31,20 +32,23 @@ RotaryEncoder encoder(PIN_ENCODER_A, PIN_ENCODER_B);
 EasyButton btnEncoder(PIN_ENCODER_BTN, BTN_DEBOUNCE, true);
 TM1651 display(PIN_LED_CLK, PIN_LED_DIO);
 
+Timer timer;
+
 bool config_mode = false;
 
 int16_t oldPos = 0;
 
-uint8_t mode = MODE_SOLID;
+uint8_t mode = MODE_RAINBOW;
 uint8_t color = COLOR_RED;
+uint8_t hue = 0;
 
-int8_t brightness = 100;
-int8_t red = 255;
-int8_t green = 0;
-int8_t blue = 0;
+uint8_t interval = 50;
+uint8_t brightness = 100;
+uint8_t red = 255;
+uint8_t green = 0;
+uint8_t blue = 0;
 
 void setup() {
-  Serial.begin(9600);
   // Setup display
   display.displaySet(BRIGHT_TYPICAL);
   // Setup encoder
@@ -52,21 +56,32 @@ void setup() {
   btnEncoder.onPressedFor(1000, onLongPressedEncoder);
   PcInt::attachInterrupt(PIN_ENCODER_A, encoderInterruptHandler, CHANGE);
   PcInt::attachInterrupt(PIN_ENCODER_B, encoderInterruptHandler, CHANGE);
-
+  // Display test
   display.displayInteger(0);
-  delay(1000);
+  delay(500);
+  // Setup leds
   FastLED.addLeds<LED_TYPE, PIN_OUTPUT, COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
-  
-  Serial.println("boot OK");
-  // brightness = EEPROM.read(0);
-  // if (brightness < 0 || brightness > 100) {
-  //   brightness = 30;
-  // }
+  // Leds test
+  setColor(255, 0, 0);
+  display.displayInteger(1);
+  FastLED.show();
+  delay(500);
+  setColor(0, 255, 0);
+  display.displayInteger(2);
+  FastLED.show();
+  delay(500);
+  setColor(0, 0, 255);
+  display.displayInteger(3);
+  FastLED.show();
+  delay(500);
+  // Initialize timer
+  timer.init(interval);
 }
 
 void loop() {
   int newPos = encoder.getPosition();
   btnEncoder.read();
+  timer.update();
   // If the encoder changed, act depending on the current state
   if (newPos != oldPos) {
     RotaryEncoder::Direction dir = encoder.getDirection();
@@ -75,24 +90,23 @@ void loop() {
         case MODE_SOLID:
           switch (color) {
             case COLOR_RED:
-              red += (int)dir;
-              red = clamp(red, 0, 255);
+              red = getValue(dir, red, 0, 255);
             break;
             case COLOR_GREEN:
-              green += (int)dir;
-              green = clamp(green, 0, 255);
+              green = getValue(dir, green, 0, 255);
             break;
             case COLOR_BLUE:
-              blue += (int)dir;
-              blue = clamp(blue, 0, 255);
+              blue = getValue(dir, blue, 0, 255);
             break;
           }
+        break;
+        case MODE_RAINBOW:
+          interval = getValue(dir, interval, 10, 180);
         break;
       }
     } else {
       // Set brightness
-      brightness += (int)dir;
-      brightness = clamp(brightness, 0, 100);
+      brightness = getValue(dir, brightness, 0, 100);
     }
     oldPos = newPos;
   }
@@ -112,22 +126,47 @@ void loop() {
           break;
         }
       break;
+      case MODE_RAINBOW:
+        display.displayInteger(interval);
+      break;
     }
   } else {
     display.displayInteger(brightness);
   }
+  if ( timer.hasFinished() ) {
+    hue++;
+    timer.init(interval);
+  }
   // Update leds
   switch (mode) {
     case MODE_SOLID:
-      leds->setRGB(red, green, blue);
+      setColor(red, green, blue);
+    break;
+    case MODE_RAINBOW:
+      fill_rainbow( leds, NUM_LEDS, hue, 7);
     break;
   }
   FastLED.setBrightness(brightness);
-  FastLED.show();  
+  FastLED.show();
 }
 
-int clamp(int n, int lower, int upper){
-  return max(lower, min(n, upper)); 
+void setColor(uint8_t red, uint8_t green, uint8_t blue) {
+  for(int i = 0; i < NUM_LEDS; i++) {   
+    leds[i] = CRGB(red, green, blue);
+  }
+}
+
+uint8_t getValue(RotaryEncoder::Direction dir, uint8_t value, uint8_t min, uint8_t max) {
+  switch (dir) {
+    case RotaryEncoder::Direction::CLOCKWISE:
+      return (value < max) ? (value + 1) : max;
+    break;
+    case RotaryEncoder::Direction::COUNTERCLOCKWISE:
+      return (value > min) ? (value - 1) : min;
+    break;
+    default:
+      return value;
+  }
 }
 
 void encoderInterruptHandler() {
@@ -150,14 +189,24 @@ void onPressedEncoder() {
           break;
         }
       break;
+      case MODE_RAINBOW:
+        config_mode = false;
+      break;
     }
   } else {
-    // TBD: Change mode
+    switch (mode) {
+      case MODE_SOLID:
+        mode = MODE_RAINBOW;
+        break;
+      case MODE_RAINBOW:
+        mode = MODE_SOLID;
+        break;
+    }
   }
 }
 
 void onLongPressedEncoder() {
-  if (!config_mode) {
+  if (! config_mode ) {
     config_mode = true;
     switch (mode) {
       case MODE_SOLID:
--- src/main.h
+++ src/main.h
@@ -2,10 +2,12 @@
 #define MAIN_h
 
 #include <Arduino.h>
+#include <RotaryEncoder.h>
 
 void setup();
 void loop();
-int clamp(int n, int lower, int upper);
+void setColor(uint8_t red,uint8_t green,uint8_t blue);
+uint8_t getValue(RotaryEncoder::Direction dir, uint8_t value, uint8_t min, uint8_t max);
 
 void onPressedEncoder();
 void onLongPressedEncoder();
